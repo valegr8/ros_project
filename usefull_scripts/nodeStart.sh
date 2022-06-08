96e71247cf7de02cfd27094b5d57fa319ea8b519
Valeria <valeria.grotto01@gmail.com>
2022-06-08 23:08:41 +0200

    sigle world

--- usefull_scripts/nodeStart.sh
+++ usefull_scripts/nodeStart.sh
@@ -1,2 +1,3 @@
-source devel/setup.bash && clear && rosrun ur5_pkg node
+source devel/setup.bash 
+rosrun ur5_pkg node
 #catkin build --env-cache && source devel/setup.bash && clear && rosrun ur5_pkg node
